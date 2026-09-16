7222a8129f8f8263163e547c3da913ac504382a4
Thabani2805 <thabaninhoks@gmail.com>
2026-09-16 20:25:00 +0200

    Add files via upload

---
+++ bank 1.py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+        print("Deposit successful.")
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print("Withdrawal successful.")
+        else:
+            print("Insufficient funds.")
+
+    def display_balance(self):
+        print("Current balance:", self.__balance)
+
+
+# Create a BankAccount object
+account = BankAccount(1000)
+
+# Deposit money
+account.deposit(500)
+
+# Withdraw money
+account.withdraw(200)
+
+# Display balance
+account.display_balance()
